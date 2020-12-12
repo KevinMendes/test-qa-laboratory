5611d6bd41ab23225afbd6871ffb809e91eea7e6
KevinMendes <mendes.kevin35@gmail.com>
2020-12-12 11:07:10 +0100

    $commit

---
+++ vstable/src/pages/home/Home.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+import './Home.scss';
+
+interface Props {
+
+}
+
+export const Home: React.FC<Props> = (props) => {
+  return <div className={"home--container"}>
+
+  </div>
+}
